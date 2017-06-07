ae50510c53136d90445f3281cafb6908bfbf5ae3
Florian Lentsch <office@florian-lentsch.at>
2017-06-07 08:53:26 +0200

    added cdvStartInBackground extra so the activity gets launched in bg

--- src/android/WakeupReceiver.java
+++ src/android/WakeupReceiver.java
@@ -26,15 +26,15 @@ public class WakeupReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Log.d(LOG_TAG, "wakeuptimer expired at " + sdf.format(new Date().getTime()));
-	
+
 		try {
 			String packageName = context.getPackageName();
 			Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
-			String className = launchIntent.getComponent().getClassName();		    	
+			String className = launchIntent.getComponent().getClassName();
 			Log.d(LOG_TAG, "launching activity for class " + className);
 
 			@SuppressWarnings("rawtypes")
-			Class c = Class.forName(className); 
+			Class c = Class.forName(className);
 
 			Intent i = new Intent(context, c);
 			i.putExtra("wakeup", true);
@@ -43,10 +43,11 @@ public class WakeupReceiver extends BroadcastReceiver {
 			if (extrasBundle!=null && extrasBundle.get("extra")!=null) {
 				extras = extrasBundle.get("extra").toString();
 			}
-			
+
 			if (extras!=null) {
 				i.putExtra("extra", extras);
 			}
+			i.putExtra("cdvStartInBackground", true);
 			i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			context.startActivity(i);
 
@@ -58,20 +59,20 @@ public class WakeupReceiver extends BroadcastReceiver {
 				}
 				PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, o);
 				pluginResult.setKeepCallback(true);
-				WakeupPlugin.connectionCallbackContext.sendPluginResult(pluginResult);  
+				WakeupPlugin.connectionCallbackContext.sendPluginResult(pluginResult);
 			}
-			
+
 			if (extrasBundle!=null && extrasBundle.getString("type")!=null && extrasBundle.getString("type").equals("daylist")) {
 				// repeat in one week
 				Date next = new Date(new Date().getTime() + (7 * 24 * 60 * 60 * 1000));
 				Log.d(LOG_TAG,"resetting alarm at " + sdf.format(next));
-	
+
 				Intent reschedule = new Intent(context, WakeupReceiver.class);
 				if (extras!=null) {
 					reschedule.putExtra("extra", intent.getExtras().get("extra").toString());
 				}
 				reschedule.putExtra("day", WakeupPlugin.daysOfWeek.get(intent.getExtras().get("day")));
-	
+
 				PendingIntent sender = PendingIntent.getBroadcast(context, 19999 + WakeupPlugin.daysOfWeek.get(intent.getExtras().get("day")), intent, PendingIntent.FLAG_UPDATE_CURRENT);
 				AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 				if (Build.VERSION.SDK_INT>=19) {
